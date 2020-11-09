e8aa8bf3f62079a3b6b8a58657fc5d027be85800
shyamj <jandhyala.shyam@tieto.com>
2020-11-09 20:50:30 +0530

    Added order page

--- server.js
+++ server.js
@@ -54,7 +54,8 @@ client.query('SELECT * FROM broker__c', function(error, data) {
 
 app.get('/order', function(req, res) {
   client.query('SELECT * FROM ' + orderTable, function(error, data) {
-    res.json(data.rows[0]);
+    console.log(data.rows);
+    res.json(data.rows);
   });
 });
 
