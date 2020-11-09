25c199bbaef973f5023ab4d85b3688d2ca515f2f
shyamj <jandhyala.shyam@tieto.com>
2020-11-09 15:57:26 +0530

    Added product page

--- server.js
+++ server.js
@@ -28,6 +28,7 @@ var productTable = 'product2';
 // setup the demo data if needed
 client.query('SELECT * FROM salesforce.broker__c', function(error, data) {
   if (error !== null) {
+    console.log('Loading Data from broker__c...');
     client.query('SELECT * FROM broker__c', function(error, data) {
       if (error !== null) {
         console.log('Loading Demo Data...');
